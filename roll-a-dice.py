585c8459d777d077df0daee957dbd815acf065e8
JoaoEduardoAndretto <125469869+JoaoEduardoAndretto@users.noreply.github.com>
2024-05-03 15:44:38 -0300

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,51 @@
+import random 
+
+response = "y"
+while response == "y":
+    number = random.randint(1,6)
+
+    if number == 1:
+        print("-----")
+        print("|   |")
+        print("| O |")
+        print("|   |")
+        print("-----")
+
+    elif number == 2:
+        print("-----")
+        print("|O  |")
+        print("|   |")
+        print("|  O|")
+        print("-----")
+
+    elif number == 3:
+        print("-----")
+        print("|O  |")
+        print("| O |")
+        print("|  O|")
+        print("-----")
+
+    elif number == 4:
+        print("-----")
+        print("|O O|")
+        print("|   |")
+        print("|O O|")
+        print("-----")
+
+    elif number == 5:
+        print("-----")
+        print("|O O|")
+        print("| O |")
+        print("|O O|")
+        print("-----")
+
+    elif number == 6:
+        print("-----")
+        print("|O O|")
+        print("|O O|")
+        print("|O O|")
+        print("-----")
+
+    response = input("Deseja jogar novamente? (y/n): ")
+
+    
